2ae6837ef7bea00cd53673ca74cbd08fa0e6d0e2
Mohamed essam <zoroloffy95@gmail.com>
2023-03-01 15:33:02 +0200

    fix google and facebook issue

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Notifications\LoginNotification;
 use Laravel\Socialite\Facades\Socialite;
 
 class FacebookController extends Controller
@@ -11,6 +15,27 @@ class FacebookController extends Controller
         return Socialite::driver('facebook')->redirect();
       }
       public function callback(){
-        $user = Socialite::driver('facebook')->stateless()->user(); 
+        try{
+          $facebookUser = Socialite::with('facebook')->stateless()->user();
+          //return response()->json($facebookUser);
+          $user = User::where('email', $facebookUser->emil)->first();
+           if ($user!=null) {
+              $user1=Auth::user();
+              $user1->tokens()->delete();
+              $user1->notify(new LoginNotification());
+              $success['token']=$user1->createToken('user',['user'])->plainTextToken;
+              $success['msg']=$user->name;
+              $success['success']=true;
+              return response()->json($success,200);
+  
+          } else {
+              return response()->json([
+                  'msg'=>"need to register"
+              ]);
+           
+          }  
+      }catch (Exception $ex) {
+          return response()->json(['status' => 'error', 'expcetion' => $ex->getMessage(), 'msg' => ' facebook failed'], 500);
+      }
       }
 }
--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -19,23 +19,28 @@ class GoogleController extends Controller
 
     public function callback()
     {
-        $googleUser = Socialite::driver('google')->stateless()->user();
-        dd($googleUser);
-        $user = User::where('email', $googleUser->emil)->first();
-        if (!$user) {
-            $user1=Auth::user();
-            $user1->tokens()->delete();
-            $user1->notify(new LoginNotification());
-            $success['token']=$user1->createToken('user',['user'])->plainTextToken;
-            $success['msg']=$user->name;
-            $success['success']=true;
-            return response()->json($success,200);
-
-        } else {
-            return response()->json([
-                'msg'=>"need to register"
-            ]);
-         
-        } 
+        try{
+            $googleUser = Socialite::with('google')->stateless()->user();
+            //return response()->json($googleUser);
+            $user = User::where('email', $googleUser->emil)->first();
+             if ($user!=null) {
+                $user1=Auth::user();
+                $user1->tokens()->delete();
+                $user1->notify(new LoginNotification());
+                $success['token']=$user1->createToken('user',['user'])->plainTextToken;
+                $success['msg']=$user->name;
+                $success['success']=true;
+                return response()->json($success,200);
+    
+            } else {
+                return response()->json([
+                    'msg'=>"need to register"
+                ]);
+             
+            }  
+        }catch (Exception $ex) {
+            return response()->json(['status' => 'error', 'expcetion' => $ex->getMessage(), 'msg' => ' google failed'], 500);
+        }
+        
     }
 }
